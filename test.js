1fa2ddcb7fb3221e97f79619f19164e82253969f
hihi12410 <csillag.marcell1809@gmail.com>
2025-03-23 16:36:33 +0100

    Did sone funny work hehe

--- test.js
+++ test.js
@@ -1,156 +1,255 @@
-    var xSize = 0;
-    var ySize = 0;
+var xSize = 0;
+var ySize = 0;
 
-    function onWindowResize(canvas) {
-        const scale = window.devicePixelRatio;
-        const width = window.innerWidth;
-        const height = window.innerHeight;
+var writeOrder = 0;
 
-        canvas.width = width * scale;
-        canvas.height = height * scale;
+function stopAllWrite() 
+{
+    writeOrder++;
+    if (writeOrder > 10000000) writeOrder = 0;
+}
 
-        xSize = canvas.width;
-        ySize = canvas.height;
+function onWindowResize(canvas) {
+    const scale = window.devicePixelRatio;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    canvas.width = width * scale;
+    canvas.height = height * scale;
+    
+    xSize = canvas.width;
+    ySize = canvas.height;
+}
+
+class VisualTextBoxStyle 
+{
+    constructor(x, y, w, h, title, image, imageSize, text, textcolor, font, fontsize, duration, backgroundGradientColor, backgroundGradientRatio, titleCardGradientRatio, ctx)
+    {
+        this.ctx = ctx;
+        
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        
+        this.title = title;
+        this.image = new Image();
+        this.image.src = image;
+        this.imageSize = imageSize;
+        
+        this.text = text;
+        this.textcolor = textcolor;
+        this.font = font;
+        this.fontsize = fontsize;
+        this.duration = duration;
+        
+        this.backgroundGradient = ctx.createLinearGradient(this.x, this.y, this.x, this.y + this.h);
+        this.backgroundGradient.addColorStop(0, backgroundGradientColor);
+        this.backgroundGradient.addColorStop(backgroundGradientRatio, "rgba(0, 0, 0, 0.5)");
+        this.backgroundGradient.addColorStop(1, "rgba(0, 0, 0, 0)");
+        
+        this.titleCardGradient = ctx.createLinearGradient(this.x, this.y, this.x, this.y - this.h);
+        this.titleCardGradient.addColorStop(0, backgroundGradientColor);
+        this.titleCardGradient.addColorStop(titleCardGradientRatio, "rgba(0, 0, 0, 0.5)");
+        this.titleCardGradient.addColorStop(1, "rgba(0, 0, 0, 0)");
+        
     }
+    
+    setText(text)
+    {
+        this.text = text;
+    }
+}
 
-    class textBox {
-        constructor(x, y, w, h, title, image, imageSize,text, textcolor, font, fontsize, duration, backgroundGradientColor, backgroundGradientRatio, titleCardGradientRatio, ctx) {
-            this.w = w;
-            this.h = h;
-            this.x = x;
-            this.y = y;
-            
-            this.text = text;
-            this.textcolor = textcolor;
-            this.duration = duration;
-            
-            this.backgroundGradientColor = backgroundGradientColor;
-            this.backgroundGradientRatio = backgroundGradientRatio;
-            this.titleCardGradientRatio = titleCardGradientRatio;
-            
-            this.font = font;
-            this.fontsize = fontsize;
-            
-            this.ctx = ctx;
-            this.ctx.font = `${this.fontsize}px ${this.font}`;            
+class actor
+{
+    constructor(name, vtbs)
+    {
+        this.name = name;
+        this.vtbs = vtbs;
+    }
+}
 
-            this.backgroundGradient = ctx.createLinearGradient(this.x, this.y, this.x, this.y + this.h);
-            this.backgroundGradient.addColorStop(0, this.backgroundGradientColor);
-            this.backgroundGradient.addColorStop(this.backgroundGradientRatio, "rgba(0, 0, 0, 0.5)");
-            this.backgroundGradient.addColorStop(1, "rgba(0, 0, 0, 0)");
-
-            this.titleCardGradient = ctx.createLinearGradient(this.x, this.y, this.x, this.y - this.h);
-            this.titleCardGradient.addColorStop(0, this.backgroundGradientColor);
-            this.titleCardGradient.addColorStop(this.titleCardGradientRatio, "rgba(0, 0, 0, 0.5)");
-            this.titleCardGradient.addColorStop(1, "rgba(0, 0, 0, 0)");
-
-            this.title = title;
-
-            this.imageSize = imageSize;
-            this.image = new Image();
-            this.image.src = image;
-            this.loaded = false;
-
-            this.image.onload = () => {
-                this.loaded = true;
-                this.show();
-            };
+class textBox {
+    constructor(vtbs) {
+        this.loaded = false;
+        this.vtbs = vtbs;
+        vtbs.abort = false;
+        
+        this.vtbs.image.onload = () => {
+            this.loaded = true;
+        };
+    }
+    
+    
+    wrapText() {
+        let words = this.vtbs.text.split(" ");
+        let lines = [];
+        let currentLine = words[0];
+        
+        for (let i = 1; i < words.length; ++i) {
+            let testLine = currentLine + " " + words[i];
+            let width = this.vtbs.ctx.measureText(testLine).width;
+            if (width > this.vtbs.w) {
+                lines.push(currentLine);
+                currentLine = words[i];
+            } else {
+                currentLine = testLine;
+            }
         }
+        lines.push(currentLine);
+        return lines;
+    }
+    
+    wrapText_monospaced() {
+        let words = this.vtbs.text.split(" ");
+        let lines = [];
+        let currentLine = words[0];
+        
+        for (let i = 1; i < words.length; ++i) {
+            let testLine = currentLine + " " + words[i];
+            let width = this.vtbs.fontsize * testLine.length;
+            if (width > this.vtbs.w) {
+                lines.push(currentLine);
+                currentLine = words[i];
+            } else {
+                currentLine = testLine;
+            }
+        }
+        lines.push(currentLine);
+        return lines;
+    }
+    
+    renderTitleCard() 
+    {
+        const imageAspectRatio = this.vtbs.image.width / this.vtbs.image.height;
+        let imageWidth = this.vtbs.imageSize;
+        let imageHeight = imageWidth / imageAspectRatio;
+        
+        if (imageHeight > this.vtbs.h) {
+            imageHeight = this.vtbs.h;
+            imageWidth = imageHeight * imageAspectRatio;
+        }
+        
+        const imageX = this.vtbs.x;
+        const imageY = this.vtbs.y - this.vtbs.imageSize;
+        
+        this.vtbs.ctx.fillStyle = this.vtbs.titleCardGradient;
+        this.vtbs.ctx.fillRect(imageX, imageY, this.vtbs.imageSize, this.vtbs.imageSize);
+        
+        this.vtbs.ctx.drawImage(this.vtbs.image, imageX, imageY, this.vtbs.imageSize, this.vtbs.imageSize);
+        
+        this.vtbs.ctx.fillStyle = this.vtbs.textcolor;
+        this.vtbs.ctx.fillText(this.vtbs.title, imageX, imageY+this.vtbs.imageSize);
+    }
+    
+    renderBackground() 
+    {
+        this.vtbs.ctx.fillStyle = this.vtbs.backgroundGradient;
+        this.vtbs.ctx.fillRect(this.vtbs.x, this.vtbs.y, this.vtbs.w, this.vtbs.h);
+    }
+    
+    renderText_allAtOnce() 
+    {
+        this.vtbs.ctx.fillStyle = this.vtbs.textcolor;
+        let lines = this.wrapText();
+        let lineHeight = this.vtbs.fontsize * 1;
+        let startY = this.vtbs.y + (this.vtbs.h - lines.length * lineHeight) / 2 + this.vtbs.fontsize;
+        
+        for (let i = 0; i < lines.length; i++) {
+            let textWidth = this.vtbs.ctx.measureText(lines[i]).width;
+            let textX = this.vtbs.x + (this.vtbs.w - textWidth) / 2;
+            this.vtbs.ctx.fillText(lines[i], textX, startY + i * lineHeight);
+        }
+    }
+    
+    renderText_typeWriter() 
+    {
+        this.vtbs.ctx.fillStyle = this.vtbs.textcolor;
+        
+        let lines = this.wrapText_monospaced();
+        var lineHeight = this.vtbs.fontsize * 1;
+        var startY = this.vtbs.y + (this.vtbs.h - lines.length * lineHeight) / 2 + this.vtbs.fontsize;
+        
+        var i = 0;
+        var j = 0;
+    
+        var curr = writeOrder
 
-        wrapText() {
-            let words = this.text.split(" ");
-            let lines = [];
-            let currentLine = words[0];
-
-            for (let i = 1; i < words.length; ++i) {
-                let testLine = currentLine + " " + words[i];
-                let width = this.ctx.measureText(testLine).width;
-                if (width > this.w) {
-                    lines.push(currentLine);
-                    currentLine = words[i];
-                } else {
-                    currentLine = testLine;
+        function write(vtbs)
+        {   
+            if(curr == writeOrder){
+                const charSize = vtbs.fontsize;
+                const textX = vtbs.x + (vtbs.w - charSize) / 2;
+                vtbs.ctx.fillText(lines[i][j], vtbs.fontsize*j, startY + i * lineHeight);
+                
+                if (i < lines.length) 
+                    {
+                    
+                    if (++j < lines[i].length && curr == writeOrder) 
+                        {
+                        setTimeout(()=>{write(vtbs);}, vtbs.delay);
+                    }else
+                    {
+                        i++; j = 0;
+                        if (i < lines.length && curr == writeOrder) 
+                            {
+                            setTimeout(()=>{write(vtbs);}, vtbs.delay);
+                        }
+                    }
                 }
-            }
-            lines.push(currentLine);
-            return lines;
-        }
-
-        renderTitleCard() 
-        {
-            const imageAspectRatio = this.image.width / this.image.height;
-            let imageWidth = this.imageSize;
-            let imageHeight = imageWidth / imageAspectRatio;
-
-            if (imageHeight > this.h) {
-                imageHeight = this.h;
-                imageWidth = imageHeight * imageAspectRatio;
-            }
-
-            const imageX = this.x;
-            const imageY = this.y - this.imageSize;
-
-            this.ctx.fillStyle = this.titleCardGradient;
-            this.ctx.fillRect(imageX, imageY, this.imageSize, this.imageSize);
-
-            this.ctx.drawImage(this.image, imageX, imageY, this.imageSize, this.imageSize);
-
-            this.ctx.fillStyle = this.textcolor;
-            this.ctx.fillText(this.title, imageX, imageY+this.imageSize);
-        }
-
-        renderBackground() 
-        {
-            this.ctx.fillStyle = this.backgroundGradient;
-            this.ctx.fillRect(this.x, this.y, this.w, this.h);
-        }
-
-        renderText_allAtOnce() 
-        {
-            this.ctx.fillStyle = this.textcolor;
-            let lines = this.wrapText();
-            let lineHeight = this.fontsize * 1;
-            let startY = this.y + (this.h - lines.length * lineHeight) / 2 + this.fontsize;
-
-            for (let i = 0; i < lines.length; i++) {
-                let textWidth = this.ctx.measureText(lines[i]).width;
-                let textX = this.x + (this.w - textWidth) / 2;
-                this.ctx.fillText(lines[i], textX, startY + i * lineHeight);
+        
             }
         }
-
-        show() {
-            //Wait for the image to be loaded
-            if (!this.loaded) {
-                //setTimeout(() => this.show(), 10);
-                return;
-            }
-
-            this.ctx.font = `${this.fontsize}px ${this.font}`;
-            this.renderBackground();
-            this.renderTitleCard();
-            this.renderText_allAtOnce();
+        write(this.vtbs);
+    }
+    
+    show() {
+        //Wait for the image to be loaded
+        if (!this.loaded) {
+            //setTimeout(() => this.show(), 10);
+            return;
         }
+
+        this.vtbs.ctx.font = `${this.vtbs.fontsize}px ${this.vtbs.font}`;
+        this.renderBackground();
+        this.renderTitleCard();
+        //this.renderText_allAtOnce();
+        this.renderText_typeWriter();
+    }
+}
+
+const testText = "I quite fancy this textbox. It looks clean and nice! It also handles overflow, and that is always nice. I really do like javascript! To make the text overflow, we either have to write more text, or make the fontsize bigger. It also enables us to make fancy gradients, just like those visual-novel games!"; 
+const testText2 = "Muhahaha! I am the evil textbox. I am nothing else, but evil. You surely are scared. I am the spawn of all evil. I eat babies for breakfast. You don't wan't to mess with me, I am sure of that. Muhahahahaha!"
+
+
+
+window.onload = function () {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    
+    window.onresize = () => onWindowResize(canvas);
+    onWindowResize(canvas);
+    
+    const vtbs = new VisualTextBoxStyle(0, ySize-300, xSize, 300, "Hihi12410", "./images/touhou.webp",300, testText, "white", "system-ui", 27, 200, "blue", .9, .9, ctx);
+    const vtbs2 = new VisualTextBoxStyle(0, ySize-300, xSize, 300, "Evil Hihi12410", "./images/duranda.jpeg",300, testText2, "white", "system-ui", 27, 200, "red", .9, .9, ctx);
+    const text = new textBox(vtbs);
+    const evilText = new textBox(vtbs2);
+
+    window.onclick = function()
+    {
+        stopAllWrite();
+        ctx.clearRect(0,0,xSize, ySize);
+        text.show();
     }
 
-    const testText = "I quite fancy this textbox. It looks clean and nice! It also handles overflow, and that is always nice. I really do like javascript! To make the text overflow, we either have to write more text, or make the fontsize bigger. It also enables us to make fancy gradients, just like those visual-novel games!"; 
-
-    function mainLoop(textBox, ctx) {
-        requestAnimationFrame(() => mainLoop(textBox, ctx));
-        
-        ctx.clearRect(0, 0, xSize, ySize);
-        
-        textBox.show();
+    window.onkeypress = function (e)
+    {
+        if (e.key == ' ') 
+            {
+            stopAllWrite();
+            ctx.clearRect(0,0,xSize, ySize);
+            evilText.show();
+        }
     }
-
-
-    window.onload = function () {
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
-
-        window.onresize = () => onWindowResize(canvas);
-        onWindowResize(canvas);
-
-        const text = new textBox(0, ySize-300, xSize, 300, "Hihi12410", "./images/touhou.webp",300, testText, "white", "system-ui", 30, 1, "blue", .9, .9, ctx);
-        mainLoop(text, ctx);
-    };
+};
